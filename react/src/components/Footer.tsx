690bba40cb5db89539746663a64ea6d96b1b54db
Dávid Szilágyi <david.skb2006@gmail.com>
2026-08-11 19:31:13 +0300

    completed footer.tsx

--- react/src/components/Footer.tsx
+++ react/src/components/Footer.tsx
@@ -1,5 +1,25 @@
+import {Link} from "react-router-dom"
+
 function Footer() {
-  return null
+  return (
+    <footer>
+      <Link to="/">Food Ninja</Link>
+      <div>
+        <Link to="/">Blog</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+      <div>
+        <span>Other ways to reach me:</span>
+        <ul>
+          <li>Email: hello@foodninja.com</li>
+          <li>Twitter: @foodninja</li>
+          <li>Instagram: @foodninja.eats</li>
+        </ul>
+      </div>
+      <div><span>© 2026 Food Ninja</span></div>
+    </footer>
+  )
 }
 
 export default Footer
